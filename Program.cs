805c97953ee31f5cad211257533b91e7c24d9f9b
Ryan Ramsey <rramsey1115@gmail.com>
2024-01-18 14:28:22 -0600

    Instructions for setup followed, with errors

--- Program.cs
+++ Program.cs
@@ -1,11 +1,60 @@
+using System.Text.Json.Serialization;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Identity;
+using Unison.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+builder.Services.AddControllers().AddJsonOptions(opts =>
+{
+    opts.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles;
+});
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+    .AddCookie(CookieAuthenticationDefaults.AuthenticationScheme, options =>
+    {
+        options.Cookie.Name = "UnisonLoginCookie";
+        options.Cookie.SameSite = SameSiteMode.Strict;
+        options.Cookie.HttpOnly = true; //The cookie cannot be accessed through JS (protects against XSS)
+        options.Cookie.MaxAge = new TimeSpan(7, 0, 0, 0); // cookie expires in a week regardless of activity
+        options.SlidingExpiration = true; // extend the cookie lifetime with activity up to 7 days.
+        options.ExpireTimeSpan = new TimeSpan(24, 0, 0); // Cookie will expire in 24 hours without activity
+        options.Events.OnRedirectToLogin = (context) =>
+        {
+            context.Response.StatusCode = StatusCodes.Status401Unauthorized;
+            return Task.CompletedTask;
+        };
+        options.Events.OnRedirectToAccessDenied = (context) =>
+        {
+            context.Response.StatusCode = StatusCodes.Status403Forbidden;
+            return Task.CompletedTask;
+        };
+    });
+
+builder.Services.AddIdentityCore<IdentityUser>(config =>
+            {
+                //for demonstration only - change these for other projects
+                config.Password.RequireDigit = false;
+                config.Password.RequiredLength = 8;
+                config.Password.RequireLowercase = false;
+                config.Password.RequireNonAlphanumeric = false;
+                config.Password.RequireUppercase = false;
+                config.User.RequireUniqueEmail = true;
+            })
+    .AddRoles<IdentityRole>()  //add the role service.  
+    .AddEntityFrameworkStores<UnisonDbContext>();
+
+// allows passing datetimes without time zone data 
+AppContext.SetSwitch("Npgsql.EnableLegacyTimestampBehavior", true);
+
+// allows our api endpoints to access the database through Entity Framework Core
+builder.Services.AddNpgsql<UnisonDbContext>(builder.Configuration["UnisonDbConnectionString"]);
+
 
 var app = builder.Build();
 
@@ -17,7 +66,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
+// these two calls are required to add auth to the pipeline for a request
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
