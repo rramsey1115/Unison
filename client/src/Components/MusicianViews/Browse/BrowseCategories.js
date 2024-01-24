1440dd24573b50f1e2fd73c90d57e44c71e3da5e
Ryan Ramsey <rramsey1115@gmail.com>
2024-01-24 14:45:43 -0600

    stduents can create,update,delete their own activities

--- client/src/Components/MusicianViews/Browse/BrowseCategories.js
+++ client/src/Components/MusicianViews/Browse/BrowseCategories.js
@@ -33,11 +33,11 @@ export const BrowseCategories = ({loggedInUser}) => {
 
     return (
         <div className="browse-container">
-            <header id="browse-category-header" className="browse-header">
+            <div id="browse-category-header" className="browse-header">
                 <h1>Browse Categories</h1>
                 {loggedInUser.roles[0]!=="Teacher" ? null
                 :<CreateCategoryModal loggedInUser={loggedInUser} getAndSetAllCategories={getAndSetAllCategories}/> }
-            </header>
+            </div>
             <section className="browse-body">
                 <Accordion open={open} toggle={toggle}>
                     {categories?.map(c => {
